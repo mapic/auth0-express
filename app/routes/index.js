ab3d35df05d70e52bd81a298b7ef17e677d08c94
knutole <knutole@mapic.io>
2019-11-07 12:43:34 +0000

    fix title

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
+  res.render('index', { title: 'NGI Shiny' });
 });
 
 
