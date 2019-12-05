40eae1732f5a1cf80e1f93359fd14535b02d9035
knutole <knutole@mapic.io>
2019-12-05 11:24:14 +0000

    cleanup

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -9,15 +9,15 @@ var router = express.Router();
 var util = require('util');
 var url = require('url');
 
+const VERIFY_EMAIL_MSG = 'Du har nå registrert en ny bruker som må verifiseres via en link tilsendt til din epostadresse. Ved spørsmål ta kontakt via <a href="mailto:miljoskytjenester@ngi.no">miljoskytjenester@ngi.no</a>.'
+const UNAUTHORIZED_MSG = 'Du er ikke autorisert til å logge inn. Ved spørsmål ta kontakt via <a href="mailto:miljoskytjenester@ngi.no">miljoskytjenester@ngi.no</a>.'
 
 dotenv.config();
-
 
 // Perform the login, after login Auth0 will redirect to callback
 router.get('/login', passport.authenticate('auth0', {
   scope: 'openid email profile'
 }), function (req, res) {
-  console.log('redirecting to /')
   res.redirect('/');
 });
 
@@ -30,36 +30,33 @@ router.get('/logs', function (req, res, next) {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
-    console.log('callback, err, user, info:', err, user, info);
     if (err) { return next(err); }
-
-    console.log('req.query: ', req.query);
-    console.log('req.params: ', req.params);
 
     // deny: not whitelisted
     if (req.query && req.query.error_description == 423) {
       res.status(423);
-      return next(new Error('Du er ikke autorisert til å logge inn. Kontakt NGI for mer informasjon.'));
+      return next(new Error(UNAUTHORIZED_MSG));
     }
 
     // deny: need to verify email
     if (req.query && req.query.error_description == 417) {
       res.status(417);
-      return next(new Error('Vennligst sjekk innboksen og verifisér eposten din før du fortsetter.'));
+      return next(new Error(VERIFY_EMAIL_MSG));
     }
 
     if (!user) { 
-      console.log('redirecting to ///login');
       return res.redirect('/login'); 
     }
+
+    // log in user
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      console.log('redirecting to: ', returnTo)
       res.redirect(returnTo || '/forside');
     });
   })(req, res, next);
+
 });
 
 
@@ -67,10 +64,12 @@ router.get('/callback', function (req, res, next) {
 
 // Perform session logout and redirect to homepage
 router.get('/logout', (req, res) => {
+
+  // log out user
   req.logout();
 
+  // define urls
   var returnTo = 'https://' + req.hostname;
-  
   var logoutURL = new url.URL(
     util.format('https://%s/logout', process.env.AUTH0_DOMAIN)
   );
@@ -80,14 +79,8 @@ router.get('/logout', (req, res) => {
   });
   logoutURL.search = searchString;
 
-  console.log('redirecting to /logout', logoutURL);
-
+  // redirect to logout url
   res.redirect(logoutURL);
 });
 
-
-
-
 module.exports = router;
-
-
